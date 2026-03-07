c8b268cab1b3ad02c75cef678eaa7dbe14ee70f9
faheemhssn <faheemhssn@gmail.com>
2026-03-07 13:34:27 -0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       payment_method_types: ['card'],
       mode: 'subscription',
       line_items: [{
-        price: process.env.STRIPE_PRICE_ID,,
+        price: process.env.STRIPE_PRICE_ID,
         quantity: 1
       }],
       customer_email: email,
